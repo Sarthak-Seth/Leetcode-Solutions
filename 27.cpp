cd54e47050dd0b61abf56dca5c23ec7b7aaf8434
Sarthak-Seth <93781378+Sarthak-Seth@users.noreply.github.com>
2023-05-20 00:51:27 +0530

    Create 27.cpp

---
+++ 27.cpp
@@ -0,0 +1,14 @@
+class Solution 
+{
+public:
+    int removeElement(vector<int>& nums, int k) 
+    {
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] != k) nums[j++] = nums[i];
+        }
+        //nums = ans;
+        return j;
+    }
+};
