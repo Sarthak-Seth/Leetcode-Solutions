ed44638853b7172291e18f6eefff21bea40d47c3
Sarthak-Seth <93781378+Sarthak-Seth@users.noreply.github.com>
2023-05-30 20:51:38 +0530

    Create 136.cpp

---
+++ 136.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        
+        int zor=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            zor^=nums[i];
+            
+        }
+        return zor;
+    }
+};
