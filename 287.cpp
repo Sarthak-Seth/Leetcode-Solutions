93f480f8a72a80c9698c6b6daebeccdef99ae6e9
Sarthak-Seth <93781378+Sarthak-Seth@users.noreply.github.com>
2023-06-05 12:56:02 +0530

    Create 287.cpp

---
+++ 287.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    
+    int findDuplicate(vector<int>& nums) 
+    {
+
+        unordered_set<int>st;
+        for(int &i:nums)
+        {
+            if(st.find(i) != st.end()) return i;
+            else st.insert(i);
+        }
+        return -1;
+    }
+};
