f1c5ae273dbb49a7057bced40f5f7e901cae9820
Sarthak-Seth <93781378+Sarthak-Seth@users.noreply.github.com>
2023-10-16 22:09:37 +0530

    Time: 13 ms (78.52%), Space: 7.6 MB (80.69%) - LeetHub

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int m = 1e9+7;
+    int dp[510][510];
+    int solve(int i, int s, int n)
+    {
+        if(i < 0 or i > n) return 0;
+        if(!s) return !i;
+        if(dp[i][s] != -1) return dp[i][s];
+
+        long long ans = 0ll+solve(i,s-1,n)%m + solve(i+1,s-1,n)%m + solve(i-1,s-1,n)%m;
+
+        return dp[i][s] = ans%m;
+ 
+    }
+    int numWays(int s, int n) 
+    {
+        memset(dp,-1,sizeof (dp));
+        return solve(0,s,n-1);
+    }
+};
