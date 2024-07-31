6bf55de20fec212c85f289ebaade980eb57c7077
Sarthak-Seth <93781378+Sarthak-Seth@users.noreply.github.com>
2024-08-01 00:36:50 +0530

    Time: 105 ms (53.27%), Space: 64.5 MB (49.8%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,35 +2,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-    let pre = [],post = [], n = nums.length,ans = [];
-    pre[0] = nums[0];
-    post[n-1] = nums[n-1];
-
-    for(let i =1;i<n;i++)
-    {
-        pre[i] = pre[i-1]*nums[i];
-        post[n-i - 1] = post[n-i]*nums[n-i - 1]
+function productExceptSelf(nums) {
+    let n = nums.length;
+    let ans = new Array(n).fill(1);
+    let curr = 1;
+    
+    for (let i = 0; i < n; i++) {
+        ans[i] *= curr;
+        curr *= nums[i];
     }
 
-    console.log(pre); 
-    console.log(post); 
-
-    ans[0] = post[1]; ans[n-1] = pre[n-2]; 
-
-    for(let k = 1;k<n-1;k++)
-    {
-        ans[k] = pre[k-1]*post[k+1];  
+    // console.log(ans);
+    
+    curr = 1;
+    
+    for (let i = n - 1; i >= 0; i--) {
+        ans[i] *= curr;
+        curr *= nums[i];
     }
-
-
-
-
-
-
-
-
-
-
+    
     return ans;
-};
+}
