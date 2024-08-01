7b389c06c1e860d94bb250a716cbde2c2d33d2ae
Sarthak-Seth <93781378+Sarthak-Seth@users.noreply.github.com>
2024-08-01 13:57:06 +0530

    Time: 89 ms (93.66%), Space: 56.4 MB (93.25%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.js
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.js
@@ -4,35 +4,22 @@
  * @return {number}
  */
 
-let dp;
-
-var solve = function(s1,s2,i,j)
-{
-    if(i === 0 || j === 0) return 0;
-
-    if(dp[i][j] !== -1) return dp[i][j];
-
-    if(s1[i-1] === s2[j-1]) return dp[i][j] = 1 + solve(s1,s2,i-1,j-1);
-
-    else return dp[i][j] = Math.max(solve(s1,s2,i-1,j),solve(s1,s2,i,j-1));
-    
-}
-
 var longestCommonSubsequence = function(s1, s2) 
 {
     const n = s1.length, m = s2.length;
-    dp = Array.from({length:s1.length+1},() => Array(s2.length + 1).fill(0));
+    let prev = Array(m+ 1).fill(0);
+    let curr = Array(m+ 1).fill(0);
 
     for(let i=1;i<n+1;i++)
     {
         for(let j=1;j<m+1;j++)
         {
-            if(s1[i-1] === s2[j-1])dp[i][j] = 1 + dp[i-1][j-1];
+            if(s1[i-1] === s2[j-1])curr[j] = 1 + prev[j-1];
 
-            else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            else curr[j] = Math.max(prev[j],curr[j-1]);
     
         }
-
+        prev = Array.from(curr);
     }
-    return dp[n][m];
+    return prev[m];
 };
